c62881351251009dd04b54ea7b662143becff60d
노정완 <jeongwan.noh@gmail.com>
2024-12-28 01:18:08 +0900

    fix: 플러그인 적용 설정 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     kotlin("jvm") version "2.1.0"
-    kotlin("plugin.spring") version "2.1.0"
-    id("org.springframework.boot") version "3.4.1"
-    id("io.spring.dependency-management") version "1.1.7"
+    kotlin("plugin.spring") version "2.1.0" apply false
+    id("org.springframework.boot") version "3.4.1" apply false
+    id("io.spring.dependency-management") version "1.1.7" apply false
 }
 
 allprojects {
