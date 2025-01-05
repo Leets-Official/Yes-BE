747599a057c477fe51c6c4e72b67526b333a0378
노정완 <jeongwan.noh@gmail.com>
2025-01-05 23:12:44 +0900

    feat: JWT Utils 구현 Jwt 생성 로직, 필터 로직, 필터 예외처리

---
+++ module-infrastructure/security/src/main/kotlin/site/yourevents/filter/JwtAuthorizationFilter.kt
@@ -0,0 +1,44 @@
+package site.yourevents.filter
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.jetbrains.annotations.NotNull
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import site.yourevents.jwt.JwtProvider
+
+@Component
+class JwtAuthorizationFilter(
+    private val jwtProvider: JwtProvider
+) : OncePerRequestFilter() {
+    companion object {
+        private const val HEADER_PREFIX: String = "Authorization"
+        private const val TOKEN_PREFIX: String = "Bearer "
+    }
+
+    override fun doFilterInternal(
+        @NotNull request: HttpServletRequest,
+        @NotNull response: HttpServletResponse,
+        @NotNull filterChain: FilterChain
+    ) {
+        val accessToken: String? = extractToken(request)
+
+        if (null != accessToken) {
+            val authentication: Authentication = jwtProvider.getAuthentication(accessToken)
+            SecurityContextHolder.getContext().authentication = authentication
+        }
+        filterChain.doFilter(request, response)
+    }
+
+    private fun extractToken(request: HttpServletRequest): String? {
+        val authorizationHeader: String = request.getHeader(HEADER_PREFIX)
+
+        if (authorizationHeader.startsWith(TOKEN_PREFIX)) {
+            return authorizationHeader.substringAfter(TOKEN_PREFIX)
+        }
+        return null
+    }
+}
