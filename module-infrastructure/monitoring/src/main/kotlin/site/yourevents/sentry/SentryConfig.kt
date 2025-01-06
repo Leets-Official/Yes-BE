1a35e05238a83a944a3c682773540e09d0e094a5
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-01-06 22:37:31 +0900

    style: 코드 컨벤션에 맞게 코드 수정

--- module-infrastructure/monitoring/src/main/kotlin/site/yourevents/sentry/SentryConfig.kt
+++ module-infrastructure/monitoring/src/main/kotlin/site/yourevents/sentry/SentryConfig.kt
@@ -7,9 +7,14 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class SentryConfig(
-    @Value("\${sentry.dsn}") private val sentryDsn: String,
-    @Value("\${sentry.environment}") private val environment: String,
-    @Value("\${sentry.servername}") private val serverName: String
+    @Value("\${sentry.dsn}")
+    private val sentryDsn: String,
+
+    @Value("\${sentry.environment}")
+    private val environment: String,
+
+    @Value("\${sentry.servername}")
+    private val serverName: String
 ) {
     @PostConstruct
     fun initSentry() {
