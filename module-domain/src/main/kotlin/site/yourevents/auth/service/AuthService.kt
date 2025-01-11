f93ad984b3b8a76e082df3f243029c98c64c7b8c
노정완 <jeongwan.noh@gmail.com>
2025-01-11 23:09:45 +0900

    style: 불필요 어노테이션 제거

--- module-domain/src/main/kotlin/site/yourevents/auth/service/AuthService.kt
+++ module-domain/src/main/kotlin/site/yourevents/auth/service/AuthService.kt
@@ -1,13 +1,11 @@
 package site.yourevents.auth.service
 
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 import site.yourevents.auth.port.`in`.usecase.AuthUseCase
 import site.yourevents.auth.port.out.SocialPort
 import site.yourevents.auth.vo.KakaoProfile
 
 @Service
-@Transactional
 class AuthService(
     private val socialPort: SocialPort
 ) : AuthUseCase {
