5d8d57421b23dfd77519fa8832ce68e4ddccab95
1winhyun <jimking1@naver.com>
2025-01-15 17:19:38 +0900

    feat: InvitationInformation 서비스 구현

--- module-domain/src/main/kotlin/site/yourevents/invitationinformation/domain/InvitationInformation.kt
+++ module-domain/src/main/kotlin/site/yourevents/invitationinformation/domain/InvitationInformation.kt
@@ -5,7 +5,7 @@ import java.time.LocalDateTime
 import java.util.UUID
 
 class InvitationInformation(
-    private val id: UUID,
+    private val id: UUID?,
     private val invitation: Invitation,
     private var title: String,
     private var schedule: LocalDateTime,
