28224b48ca414898aca1dca7878a8407d01426eb
1winhyun <jimking1@naver.com>
2025-01-15 11:48:27 +0900

    refactor: 주석 삭제

--- module-domain/src/main/kotlin/site/yourevents/invitation/service/InvitationService.kt
+++ module-domain/src/main/kotlin/site/yourevents/invitation/service/InvitationService.kt
@@ -17,8 +17,6 @@ class InvitationService(
         val member = memberUseCase.findById(memberId)
             ?: throw MemberNotFountException()
 
-        //println("Creating invitation for member: ${member.getId()}, qrUrl: $qrUrl") // 트러블 슈팅 때문에 로그 찍기 위해 넣어두었습니다.
-
         val invitation = Invitation(
             member = member,
             qrUrl = qrUrl
