09f537aa08c2582b8801f5b1e0bc3c8cda17cda1
노정완 <jeongwan.noh@gmail.com>
2025-01-20 22:59:53 +0900

    feat: 초대장 응답 로직 구현

--- module-domain/src/main/kotlin/site/yourevents/guest/port/out/GuestPersistencePort.kt
+++ module-domain/src/main/kotlin/site/yourevents/guest/port/out/GuestPersistencePort.kt
@@ -2,7 +2,12 @@ package site.yourevents.guest.port.out
 
 import site.yourevents.guest.domain.Guest
 import site.yourevents.guest.domain.GuestVO
+import java.util.UUID
 
 interface GuestPersistencePort {
     fun save(guestVo: GuestVO): Guest
+
+    fun save(guest: Guest)
+
+    fun findById(guestId: UUID): Guest?
 }
