6d99600a91346a0801b4bc887be0f5239418a2a8
노정완 <jeongwan.noh@gmail.com>
2025-01-20 22:58:01 +0900

    test: GuestService 테스트 코드 작성

--- module-domain/src/test/kotlin/site/yourevents/guest/service/GuestServiceTest.kt
+++ module-domain/src/test/kotlin/site/yourevents/guest/service/GuestServiceTest.kt
@@ -98,6 +98,75 @@ class GuestServiceTest : DescribeSpec({
                     member = member,
                     invitation = invitation,
                     nickname = guestNickname,
+                    attendance = attendance
+                )
+
+                guestService.respondInvitation(
+                    guestId = null,
+                    invitationId = invitationId,
+                    memberId = memberId,
+                    nickname = guestNickname,
+                    attendance = attendance
+                )
+
+                verify(exactly = 1) { memberUseCase.findById(memberId) }
+                verify(exactly = 1) { invitationUseCase.findById(invitationId) }
+                verify(exactly = 1) {
+                    guestPersistencePort.save(match { guestVO: GuestVO ->
+                        guestVO.member == member &&
+                                guestVO.invitation == invitation &&
+                                guestVO.nickname == guestNickname &&
+                                guestVO.attendance == attendance
+                    })
+                }
+                confirmVerified(memberUseCase, invitationUseCase, guestPersistencePort)
+            }
+
+            it("guestId가 null이 아니면 기존 Guest의 attendance만 업데이트한다.") {
+                val guestId = UUID.randomUUID()
+                val expectedGuest = Guest(
+                    id = guestId,
+                    member = member,
+                    invitation = invitation,
+                    nickname = guestNickname,
+                    attendance = true
+                )
+
+                every { guestPersistencePort.findById(any()) } returns expectedGuest
+
+                val updatedAttendance = false
+                expectedGuest.updateAttendance(updatedAttendance)
+
+                val slotGuest = slot<Guest>()
+                every { guestPersistencePort.save(capture(slotGuest)) } just runs
+
+                guestService.respondInvitation(
+                    guestId = guestId,
+                    invitationId = invitationId,
+                    memberId = memberId,
+                    nickname = guestNickname,
+                    attendance = updatedAttendance
+                )
+
+                slotGuest.captured.attendance shouldBe updatedAttendance
+
+                verify(exactly = 1) { memberUseCase.findById(memberId) }
+                verify(exactly = 1) { invitationUseCase.findById(invitationId) }
+                verify(exactly = 1) { guestPersistencePort.findById(any()) }
+                verify(exactly = 1) { guestPersistencePort.save(any<Guest>()) }
+                confirmVerified(memberUseCase, invitationUseCase, guestPersistencePort)
+            }
+        }
+
+        context("respondInvitation 메서드를 통해서") {
+            it("guestId가 null이면 새로운 Guest를 생성한다.") {
+                val attendance = true
+
+                every { guestPersistencePort.save(any<GuestVO>()) } returns Guest(
+                    id = UUID.randomUUID(),
+                    member = member,
+                    invitation = invitation,
+                    nickname = guestNickname,
                     attendance = attendance,
                     createdAt = LocalDateTime.now(),
                     modifiedAt = LocalDateTime.now()
