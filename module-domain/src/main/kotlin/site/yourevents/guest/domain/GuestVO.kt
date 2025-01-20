4c58d7a019762afe586107823cb6c87f7a0f84eb
노정완 <jeongwan.noh@gmail.com>
2025-01-20 17:56:39 +0900

    fix: 불필요 메서드 제거 및 컨벤션 수정

--- module-domain/src/main/kotlin/site/yourevents/guest/domain/GuestVO.kt
+++ module-domain/src/main/kotlin/site/yourevents/guest/domain/GuestVO.kt
@@ -2,20 +2,10 @@ package site.yourevents.guest.domain
 
 import site.yourevents.invitation.domain.Invitation
 import site.yourevents.member.domain.Member
-import java.util.UUID
 
 data class GuestVO(
     val member: Member,
     val invitation: Invitation,
     val nickname: String,
     val attendance: Boolean
-) {
-    companion object {
-        fun from(guestVO: GuestVO): GuestVO = GuestVO(
-            member = guestVO.member,
-            invitation = guestVO.invitation,
-            nickname = guestVO.nickname,
-            attendance = guestVO.attendance
-        )
-    }
-}
+)
--- module-infrastructure/persistence-db/src/main/kotlin/site/yourevents/guest/entity/GuestEntity.kt
+++ module-infrastructure/persistence-db/src/main/kotlin/site/yourevents/guest/entity/GuestEntity.kt
@@ -1,6 +1,5 @@
 package site.yourevents.guest.entity
 
-import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
