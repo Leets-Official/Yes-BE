7c69e4f9317dec9c28a34efe5483315c36af5b88
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-01-21 01:59:34 +0900

    chore: 누락된 domain 의존성 추가

--- module-infrastructure/qr/build.gradle.kts
+++ module-infrastructure/qr/build.gradle.kts
@@ -7,6 +7,8 @@ bootJar.enabled = false
 jar.enabled = true
 
 dependencies {
+    implementation(project(":module-domain"))
+
     implementation("com.google.zxing:javase:3.5.0")
     implementation("com.google.zxing:core:3.5.0")
 }
