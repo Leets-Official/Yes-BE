d1931f3c84027072c30d9e8b37a44e95fa2b07cd
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-01-22 00:08:40 +0900

    feat: QR 이미지 Url을 조회하는 API 구현

--- module-presentation/src/main/kotlin/site/yourevents/invitation/api/InvitationApi.kt
+++ module-presentation/src/main/kotlin/site/yourevents/invitation/api/InvitationApi.kt
@@ -2,12 +2,16 @@ package site.yourevents.invitation.api
 
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import site.yourevents.invitation.dto.request.CreateInvitationRequest
 import site.yourevents.invitation.dto.response.CreateInvitationResponse
+import site.yourevents.invitation.dto.response.InvitationQrResponse
 import site.yourevents.principal.AuthDetails
 import site.yourevents.response.ApiResponse
+import java.util.UUID
 
 interface InvitationApi {
     @Operation(summary = "초대장 생성")
@@ -16,4 +20,10 @@ interface InvitationApi {
         @RequestBody createInvitationRequest: CreateInvitationRequest,
         @AuthenticationPrincipal authDetails: AuthDetails
     ): ApiResponse<CreateInvitationResponse>
+
+    @Operation(summary = "초대장 QR 코드 조회")
+    @GetMapping("/invitation/QR")
+    fun getQrCode(
+        @RequestParam invitationId: UUID,
+    ): ApiResponse<InvitationQrResponse>
 }
