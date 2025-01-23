fd65ca74b78d48b196f2cc4e4cacd52da11fa94f
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-01-23 15:07:29 +0900

    feat: 초대장 개수 조회 JPQL 쿼리 작성
    - 받은 초대장 개수
    - 보낸 초대장 개수

--- module-infrastructure/persistence-db/src/main/kotlin/site/yourevents/guest/repository/GuestJPARepository.kt
+++ module-infrastructure/persistence-db/src/main/kotlin/site/yourevents/guest/repository/GuestJPARepository.kt
@@ -1,8 +1,25 @@
 package site.yourevents.guest.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import site.yourevents.guest.entity.GuestEntity
+import site.yourevents.member.entity.MemberEntity
 import java.util.UUID
 
 interface GuestJPARepository : JpaRepository<GuestEntity, UUID> {
+    @Query(
+        "SELECT COUNT(g) " +
+                "FROM guest g " +
+                "WHERE g.member = :member " +
+                "AND g.invitation.member <> :member"
+    )
+    fun getReceivedInvitationCount(member: MemberEntity): Int
+
+    @Query(
+        "SELECT COUNT(g) " +
+                "FROM guest g " +
+                "WHERE g.member = :member " +
+                "AND g.invitation.member = :member"
+    )
+    fun getSentInvitationCount(member: MemberEntity): Int
 }
