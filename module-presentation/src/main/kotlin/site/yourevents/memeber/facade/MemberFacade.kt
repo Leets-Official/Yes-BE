b2aa4a868a9d0b669a3cb04546e5b5cc5c7a4ab8
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-01-23 22:02:32 +0900

    feat: 사용자가 보낸 초대장의 정보를 조회하는 기능 구현

--- module-presentation/src/main/kotlin/site/yourevents/memeber/facade/MemberFacade.kt
+++ module-presentation/src/main/kotlin/site/yourevents/memeber/facade/MemberFacade.kt
@@ -3,18 +3,28 @@ package site.yourevents.memeber.facade
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 import site.yourevents.guest.port.`in`.GuestUseCase
+import site.yourevents.invitation.dto.response.InvitationInfoResponse
+import site.yourevents.invitation.dto.response.MyPageInvitationInfoResponse
 import site.yourevents.invitation.port.`in`.InvitationUseCase
+import site.yourevents.invitationinformation.exception.InvitationInformationNotFoundException
+import site.yourevents.invitationinformation.port.`in`.InvitationInformationUseCase
+import site.yourevents.invitationthumnail.exception.InvitationThumbnailNotFoundException
+import site.yourevents.invitationthumnail.port.`in`.InvitationThumbnailUseCase
+import site.yourevents.member.domain.MemberVO
 import site.yourevents.member.exception.MemberNotFountException
 import site.yourevents.member.port.`in`.MemberUseCase
 import site.yourevents.memeber.dto.response.MemberInfoResponse
 import site.yourevents.principal.AuthDetails
+import java.util.stream.Collectors
 
 @Service
 @Transactional
 class MemberFacade(
     private val memberUseCase: MemberUseCase,
     private val guestUseCase: GuestUseCase,
-    private val invitationUseCase: InvitationUseCase
+    private val invitationUseCase: InvitationUseCase,
+    private val invitationInformationUseCase: InvitationInformationUseCase,
+    private val invitationThumbnailUseCase: InvitationThumbnailUseCase,
 ) {
     fun getMemberInfo(authDetails: AuthDetails): MemberInfoResponse {
         val memberId = authDetails.uuid
@@ -27,5 +37,28 @@ class MemberFacade(
         val sentInvitationCount = invitationUseCase.getSentInvitationCount(member)
 
         return MemberInfoResponse.of(member.getNickname(), receivedInvitationCount, sentInvitationCount)
+    }
+
+    fun getSentInvitations(authDetails: AuthDetails): List<MyPageInvitationInfoResponse> {
+        val memberId = authDetails.uuid
+
+        val member = memberUseCase.findById(memberId)
+            ?: throw MemberNotFountException()
+
+        val sentInvitations = invitationUseCase.getSentInvitations(member)
+
+        return sentInvitations.stream()
+            .map { invitation ->
+                val invitationInfo = invitationInformationUseCase.findByInvitation(invitation)
+                    ?: throw InvitationInformationNotFoundException()
+
+                val invitationThumbnail = invitationThumbnailUseCase.findByInvitation(invitation)
+                    ?: throw InvitationThumbnailNotFoundException()
+
+                val isSender = MemberVO.from(invitation.member) == MemberVO.from(member)
+
+                MyPageInvitationInfoResponse.of(isSender, InvitationInfoResponse.of(invitation, invitationInfo, invitationThumbnail))
+            }
+            .collect(Collectors.toList())
     }
 }
