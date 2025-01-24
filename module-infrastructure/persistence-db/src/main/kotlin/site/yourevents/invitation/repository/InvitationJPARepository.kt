469f79f936e9fdcba5ff053ef830f366110ec34e
1winhyun <jimking1@naver.com>
2025-01-24 12:00:40 +0900

    refactor: Id를 통한 Invitation 검색 deleted가 false인 조건 추가

--- module-infrastructure/persistence-db/src/main/kotlin/site/yourevents/invitation/repository/InvitationJPARepository.kt
+++ module-infrastructure/persistence-db/src/main/kotlin/site/yourevents/invitation/repository/InvitationJPARepository.kt
@@ -1,9 +1,12 @@
 package site.yourevents.invitation.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import site.yourevents.invitation.domain.Invitation
 import site.yourevents.invitation.entity.InvitationEntity
 import java.util.UUID
 
 interface InvitationJPARepository : JpaRepository<InvitationEntity, UUID> {
+    @Query("SELECT i FROM invitation i where i.id = :id AND i.deleted = false")
+    fun findByIdNotDeleted(id: UUID): InvitationEntity?
 }
