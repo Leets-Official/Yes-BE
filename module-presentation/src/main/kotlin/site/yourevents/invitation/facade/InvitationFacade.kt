a6e65f49e04535b7955306863ee16194523200cf
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-01-27 16:35:04 +0900

    feat: 초대장 조회 기능 구현

--- module-presentation/src/main/kotlin/site/yourevents/invitation/facade/InvitationFacade.kt
+++ module-presentation/src/main/kotlin/site/yourevents/invitation/facade/InvitationFacade.kt
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Service
 import site.yourevents.guest.port.`in`.GuestUseCase
 import site.yourevents.invitation.dto.request.CreateInvitationRequest
 import site.yourevents.invitation.dto.response.CreateInvitationResponse
+import site.yourevents.invitation.dto.response.InvitationInfoResponse
 import site.yourevents.invitation.dto.response.InvitationQrResponse
-import site.yourevents.invitation.exception.InvitationNotFoundException
 import site.yourevents.invitation.exception.UnauthorizedException
 import site.yourevents.invitation.port.`in`.InvitationUseCase
 import site.yourevents.invitationinformation.port.`in`.InvitationInformationUseCase
@@ -46,13 +46,22 @@ class InvitationFacade(
         authDetails: AuthDetails
     ) {
         val invitation = invitationUseCase.findById(invitationId)
-            ?: throw InvitationNotFoundException()
 
         if(invitation.member.getId() != authDetails.uuid){
             throw UnauthorizedException()
         }
 
         invitationUseCase.markInvitationAsDeleted(invitationId)
+    }
+
+    fun getInvitation(invitationId: UUID): InvitationInfoResponse {
+        val invitation = invitationUseCase.findById(invitationId)
+
+        val invitationInformation = invitationInformationUseCase.findByInvitation(invitation)
+
+        val invitationThumbnail = invitationThumbnailUseCase.findByInvitation(invitation)
+
+        return InvitationInfoResponse.of(invitation, invitationInformation, invitationThumbnail)
     }
 
     private fun generateInvitation(memberId: UUID) =
