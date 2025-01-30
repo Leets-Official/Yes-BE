bc4851e40ab1b522dc84e2fe82aa879a5d5991c3
1winhyun <jimking1@naver.com>
2025-01-30 21:03:44 +0900

    refactor: JPA 쿼리문 수정

--- module-infrastructure/persistence-db/src/main/kotlin/site/yourevents/guest/repository/GuestJPARepository.kt
+++ module-infrastructure/persistence-db/src/main/kotlin/site/yourevents/guest/repository/GuestJPARepository.kt
@@ -27,9 +27,17 @@ interface GuestJPARepository : JpaRepository<GuestEntity, UUID> {
     )
     fun getReceivedInvitations(memberEntity: MemberEntity): List<InvitationEntity>
 
-    @Query("SELECT g FROM guest g WHERE g.invitation = :invitationEntity AND g.attendance = true")
+    @Query("SELECT g " +
+        "FROM guest g " +
+        "WHERE g.invitation = :invitationEntity " +
+        "AND g.attendance = true " +
+        "AND g.invitation.deleted = false")
     fun findAttendGuestsByInvitation(invitationEntity: InvitationEntity): List<GuestEntity>
 
-    @Query("SELECT g FROM guest g WHERE g.invitation = :invitationEntity AND g.attendance = false")
+    @Query("SELECT g " +
+                "FROM guest g " +
+                "WHERE g.invitation = :invitationEntity " +
+                "AND g.attendance = false " +
+                "AND g.invitation.deleted = false")
     fun findNotAttendGuestsByInvitation(invitationEntity: InvitationEntity): List<GuestEntity>
 }
