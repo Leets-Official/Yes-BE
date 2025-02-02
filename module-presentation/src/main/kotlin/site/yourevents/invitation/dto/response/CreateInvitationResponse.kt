c9403fa40c16c9ebeaae8da4c023b45dd227188c
1winhyun <jimking1@naver.com>
2025-02-02 23:59:41 +0900

    refactor: 컨벤션 유지

--- module-presentation/src/main/kotlin/site/yourevents/invitation/dto/response/CreateInvitationResponse.kt
+++ module-presentation/src/main/kotlin/site/yourevents/invitation/dto/response/CreateInvitationResponse.kt
@@ -3,10 +3,7 @@ package site.yourevents.invitation.dto.response
 import site.yourevents.invitation.domain.Invitation
 import java.util.UUID
 
-data class CreateInvitationResponse(
-    val invitationId: UUID,
-) {
-
+data class CreateInvitationResponse(val invitationId: UUID) {
     companion object {
         fun from(invitation: Invitation): CreateInvitationResponse =
             CreateInvitationResponse(invitation.id)
